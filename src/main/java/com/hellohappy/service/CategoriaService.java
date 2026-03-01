067146ae5a374a5cb29580d06c327054f6e3a903
João Antonio <jamoraes02@gmail.com>
2026-03-01 18:58:26 -0300

    feat: add CategoriaService

---
+++ src/main/java/com/hellohappy/service/CategoriaService.java
@@ -0,0 +1,27 @@
+package com.hellohappy.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.hellohappy.model.Categoria;
+import com.hellohappy.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public List<Categoria> listarTodas() {
+        return categoriaRepository.findAll();
+    }
+
+    public Optional<Categoria> buscarPorId(Long id) {
+        return categoriaRepository.findById(id);
+    }
+}
